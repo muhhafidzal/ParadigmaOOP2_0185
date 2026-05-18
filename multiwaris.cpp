58e7a8c07cf9f5ffd2f2414da3c808887661c884
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-18 15:26:43 +0700

    menambahkan class pelajar : virtual public orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -19,3 +19,12 @@ public:
         cout << "Pekerja dibuat\n" << endl;
     }
 };
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "Pelajar dibuat\n " << endl;
+    }
+};
