813db54e31409616ddf39eb92d4046cc58ba9abf
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-18 15:14:41 +0700

    menambahkan class remoteLampu

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNO[5];
+public:
+    void setSaklarNo (int i, string value) {
+        saklarNO[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNO[i];
+    }
+};
