d58c4297e494f26e6f6dc0a7db1556bbe888d7bd
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-18 15:36:54 +0700

    menambahkan class pelajar : public orang

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -20,3 +20,11 @@ public:
         cout << "pekerja dibuat\n" << endl;
     }
 };
+class pelajar : public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n " << endl;
+    }
+};
