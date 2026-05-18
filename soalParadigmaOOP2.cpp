96187ad06a08933ef2c87d02ff1a58db09f71f11
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-18 16:34:07 +0700

    menambahkan class RekeningBank

--- soalParadigmaOOP2.cpp
+++ soalParadigmaOOP2.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class RekeningBank {
+protected:
+    string nomorRekening;
+    string namaNasabah;
+    double saldo;
+
+public:
+    RekeningBank(string noRek, string nama, double saldoAwal)
+        : nomorRekening(noRek), namaNasabah(nama), saldo(saldoAwal) {}
+
+    virtual ~RekeningBank() {}
+
+    virtual void potongAdmin() = 0;
+
+    void tampilkan() {
+        cout << "Nama: " << namaNasabah
+             << "No Rek: " << nomorRekening
+             << "Saldo: Rp " << saldo << endl;
+             
+    }
+};
